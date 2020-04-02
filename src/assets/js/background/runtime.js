aa0d683d56ff94b96225fcafa17b6149796df109
Stian Haklev <stian@minervaproject.com>
2020-04-02 09:50:30 +0200

    WIP

--- src/assets/js/background/runtime.js
+++ src/assets/js/background/runtime.js
@@ -5,7 +5,7 @@ function save(highlights) {
     `<html><head>
     <meta charset="utf-8">
     <title>Instapaper Highlights Export</title>
-    <style type="text/css"> 
+    <style type="text/css">
       body {
         max-width: 800px;
         margin: 50px auto;
@@ -34,28 +34,20 @@ function save(highlights) {
         width: 100%;
         font-family: monospace;
       }
-      .roam-export {
-        width: 100%;
-        font-family: monospace;
-      }
     </style>
   </head><body>
-  <a href="#html">HTML</a> &bull; <a href="#json">JSON</a> &bull; <a href="#roam">Roam</a>
+  <a href="#html">HTML</a> &bull; <a href="#json">JSON</a>
   <hr />
-  Extension by <a href="http://www.sawyerh.com">Sawyer Hollenshead</a>
+  Extension by Stian Haklev
   <hr />
   <h1 id="html">HTML Export</h1>`
   ];
 
   highlights.forEach(entry => {
-    let part = `<h2><a href="${entry.source}" class="title">${
-      entry.title
-    }</a></h2>
+    let part = `<h2><a href="${entry.source}" class="title">${entry.title}</a></h2>
                 <blockquote class="highlight">${entry.highlight}</blockquote>`;
     if (entry.note)
-      part += `<small class="note"><strong>Note:</strong> ${
-        entry.note
-      }</small>`;
+      part += `<small class="note"><strong>Note:</strong> ${entry.note}</small>`;
     htmlParts.push(part);
   });
 
@@ -65,42 +57,6 @@ function save(highlights) {
     )}</textarea>`
   );
   htmlParts.push("</body></html>");
-  // Roam
-  htmlParts.push(
-    `<h1 id="roam">Roam Export</h1><textarea class="roam-export" rows="10">`
-  );
-  const docs = {};
-  highlights.forEach(entry => {
-    if (!docs[entry.source]) {
-      docs[entry.source] = { entry, text: "" };
-    }
-    if (entry.highlight) {
-      let text = "";
-      const h = entry.highlight.trim().split(/\n+/);
-      if (h.length === 1) {
-        text = `  - ${h[0]}`;
-      } else {
-        text = `  - ${h[0]}\n${h
-          .slice(1)
-          .map(x => `    - ${x}\n`)
-          .join("")}`;
-      }
-      docs[entry.source].text += `${text}\n${
-        entry.note ? `    - ^^${entry.note}^^ #n\n` : ""
-      }`;
-    } else if (entry.note) {
-      docs[entry.source].text += `  - ^^${entry.note}^^ #n`;
-    }
-  });
-  Object.keys(docs).forEach(source => {
-    htmlParts.push(
-      `- [[${docs[source].entry.title}]] - [link](${
-        docs[source].entry.source
-      }) #instapaper\n${docs[source].text}`
-    );
-  });
-
-  htmlParts.push("</textarea></body></html>");
 
   const blob = new Blob(htmlParts, { type: "text/html" });
   const size = blob.size + 1024 / 2;
--- src/assets/js/content-script.js
+++ src/assets/js/content-script.js
@@ -1,31 +1,68 @@
-'use strict';
-import $ from 'jquery';
+"use strict";
+import $ from "jquery";
 let highlights = [];
 let page = 1;
 
+function processJSON(highlights) {
+  const docs = {};
+  highlights.forEach(entry => {
+    if (!docs[entry.source]) {
+      docs[entry.source] = { entry, text: "" };
+    }
+    if (entry.highlight) {
+      let text = "";
+      const h = entry.highlight.trim().split(/\n+/);
+      if (h.length === 1) {
+        text = `  - ${h[0]}`;
+      } else {
+        text = `  - ${h[0]}\n${h
+          .slice(1)
+          .map(x => `    - ${x}\n`)
+          .join("")}`;
+      }
+      docs[entry.source].text += `${text}\n${
+        entry.note ? `    - ^^${entry.note}^^ #n\n` : ""
+      }`;
+    } else if (entry.note) {
+      docs[entry.source].text += `  - ^^${entry.note}^^ #n`;
+    }
+  });
+  let roamOutput = [];
+  Object.keys(docs).forEach(source => {
+    roamOutput.push(
+      `- [[${docs[source].entry.title}]] - [link](${
+        docs[source].entry.source
+      }) #instapaper\n${docs[source].text}`
+    );
+  });
+  navigator.clipboard.writeText(roamOutput.join("\n"));
+  // can't display this immediately, because it will stop the clipboard from working
+  window.setTimeout(() => window.alert("Clippings copied to clipboard"), 2000);
+}
+
 function scrapePage(parent) {
-  let articles = parent.querySelectorAll('.articles .highlight_item');
-  let nextPage = parent.querySelector('.paginate_older');
-  let progress = document.querySelector('.instapaper-exporter-progress');
+  let articles = parent.querySelectorAll(".articles .highlight_item");
+  let nextPage = parent.querySelector(".paginate_older");
+  let progress = document.querySelector(".instapaper-exporter-progress");
   progress.textContent = `Exporting highlights (Page ${page})...`;
 
   for (var i = 0; i < articles.length; i++) {
     let article = articles[i];
-    let titleNode = article.querySelector('.article_title');
-    let textNode = article.querySelector('.js_highlight_text');
-    let noteNode = article.querySelector('.highlight_comment .comment_text');
-    let linkNode = article.querySelector('.host .js_domain_linkout');
+    let titleNode = article.querySelector(".article_title");
+    let textNode = article.querySelector(".js_highlight_text");
+    let noteNode = article.querySelector(".highlight_comment .comment_text");
+    let linkNode = article.querySelector(".host .js_domain_linkout");
 
     highlights.push({
       title: titleNode.textContent.trim(),
       highlight: textNode.textContent.trim(),
       note: noteNode ? noteNode.textContent.trim() : null,
-      source: linkNode ? linkNode.getAttribute('href') : null
+      source: linkNode ? linkNode.getAttribute("href") : null
     });
   }
 
   if (nextPage) {
-    scrapeNextPage(nextPage.getAttribute('href'));
+    scrapeNextPage(nextPage.getAttribute("href"));
   } else {
     sendHighlights(highlights);
   }
@@ -33,25 +70,21 @@ function scrapePage(parent) {
 
 function scrapeNextPage(url) {
   page++;
-  let $box = $('<div />');
+  let $box = $("<div />");
   $box.load(`${url} #main_content`, () => {
     scrapePage($box.get(0));
   });
 }
 
 function sendHighlights(highlights) {
-  let progress = document.querySelector('.instapaper-exporter-progress');
+  let progress = document.querySelector(".instapaper-exporter-progress");
   document.body.removeChild(progress);
-
-  chrome.runtime.sendMessage(chrome.i18n.getMessage('@@extension_id'), {
-    message: 'save_highlights',
-    highlights: highlights
-  });
+  processJSON(highlights);
 }
 
 function showProgress() {
-  let style = document.createElement('style');
-  let progress = document.createElement('div');
+  let style = document.createElement("style");
+  let progress = document.createElement("div");
 
   style.textContent = `.instapaper-exporter-progress {
     animation: show 300ms ease;
@@ -73,14 +106,14 @@ function showProgress() {
     to { transform: translate3d(0, 0, 0); }
   }`;
 
-  progress.classList.add('instapaper-exporter-progress');
+  progress.classList.add("instapaper-exporter-progress");
 
   document.head.appendChild(style);
   document.body.appendChild(progress);
 }
 
 function handleExtensionMessage(request) {
-  if (request.message === 'scrape_page') {
+  if (request.message === "scrape_page") {
     showProgress();
     scrapePage(document);
   }
